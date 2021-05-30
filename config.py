da93ff1373f947945a4592c8cdbae49d3ed2fe25
Den_Varenik <varenik.denisua@gmail.com>
2021-05-30 16:29:48 +0300

    Change congif debug

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ BASE_DIR = path.abspath(path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = environ.get("SECRET_KEY") or 'grocceris'
-    DEBUG = environ.get('DEBUG') or True
+    DEBUG = environ.get('DEBUG') or False
     MONGO_URI = "mongodb+srv://adrii-den:n7GylHu2Yi2dvBwk@groceries.0eojr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
