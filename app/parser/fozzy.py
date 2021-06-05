8e8f58fe6a57a0e28e2e7c2b1641bd9c7b5d898f
Den_Varenik <varenik.denisua@gmail.com>
2021-06-06 00:56:01 +0300

    Create cron

--- app/parser/fozzy.py
+++ app/parser/fozzy.py
@@ -1,17 +1,16 @@
 from app.parser import get_soup, BASE_URL, get_weight_and_units, price_per_kg, get_date
 from app.parser import db
 
-CATEGORY = {
-    "corn": '300149-kukuruza',
-    "buckwheat": '300143-krupa-grechnevaya',
-    "rice": '300152-ris',
-    "barley": '300148-krupa-yachnevaya',
-    "wheat": '300147-krupa-pshenichnaya',
-}
 
-
-def get_start(category):
-    for type_good, num in category.items():
+def get_start():
+    CATEGORY = {
+        "corn": '300149-kukuruza',
+        "buckwheat": '300143-krupa-grechnevaya',
+        "rice": '300152-ris',
+        "barley": '300148-krupa-yachnevaya',
+        "wheat": '300147-krupa-pshenichnaya',
+    }
+    for type_good, num in CATEGORY.items():
         for i in range(1, 11):
             link = f'{BASE_URL["fozzy"]}/{num}?page={i}'
             soup = get_soup(link)
@@ -65,4 +64,4 @@ def get_price(good):
 
 
 if __name__ == "__main__":
-    get_start(CATEGORY)
+    get_start()
---
+++ cron.py
@@ -0,0 +1,8 @@
+from app.parser import auchan, fozzy, metro, tavriav
+
+
+if __name__ == '__main__':
+    auchan.get_start()
+    fozzy.get_start()
+    metro.get_start()
+    tavriav.get_start()
