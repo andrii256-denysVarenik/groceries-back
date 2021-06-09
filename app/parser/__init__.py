b396b7379aaebf8dc81b2bfaaca1c2cdfeaa65f6
Den_Varenik <varenik.denisua@gmail.com>
2021-06-10 01:38:07 +0300

    Create parser 2.0

--- app/parser/__init__.py
+++ app/parser/__init__.py
@@ -1,57 +1,59 @@
+from selenium import webdriver
+from fake_useragent import UserAgent
+from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
-from requests import get
-import re
-from datetime import datetime, date, time
 from app.database.provider import DbProvider
-
-db = DbProvider()
-
-
-BASE_URL = {
-    'tavriav': 'https://tavriav.ua',
-    'auchan': 'https://auchan.zakaz.ua',
-    'metro': 'https://metro.zakaz.ua',
-    'fozzy': 'https://fozzyshop.ua',
-    'atb': 'https://zakaz.atbmarket.com/',
-}
+from datetime import datetime, date, time
+from re import search
 
 
-def get_date():
-    return datetime.combine(date.today(), time.min)
+class Parser(object):
+
+    def __init__(self):
+        self._options = webdriver.ChromeOptions()
+
+        self._options.add_argument('headless')
+        self._options.add_argument(f'user-agent={UserAgent().random}')
+
+        self.__browser = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=self._options)
+        self._provider = DbProvider()
+
+    def _get_soup(self, link: str):
+        self.__browser.get(link)
+        html = self.__browser.page_source
+        self.__browser.quit()
+        return BeautifulSoup(html, 'html.parser')
+
+    @staticmethod
+    def _get_date():
+        return datetime.combine(date.today(), time.min)
+
+    @staticmethod
+    def _find_w_and_u(name_good: str):
+        return search(r'(\d*х*\**x*\d*,*\.*\d+)\s*(к*г)', name_good)
+
+    @staticmethod
+    def _weight_to_float(weight: str):
+        weight = weight.replace(',', '.')
+        try:
+            weight = float(weight)
+        except:
+            if weight.find('х') != -1:
+                x, y = weight.split('х')
+            elif weight.find('x') != -1:
+                x, y = weight.split('x')
+            elif weight.find('*') != -1:
+                x, y = weight.split('*')
+            else:
+                return
+            weight = float(x) * float(y)
+        return weight
+
+    def run(self):
+        pass
 
 
-def get_soup(url: str):
-    return BeautifulSoup(get(url).text, 'html.parser')
-
-
-def get_weight(good) -> str:
-    return good.find("div", {"class": "ProductTile__weight"}).string
-
-
-def get_weight_and_units(name_good) -> list:
-    weight = re.search(r'(\d*х*\**x*\d*,*\.*\d+)\s*(к*г)', name_good)
-    weight, units = [weight.group(1), weight.group(2)] if weight else ['1', 'кг']
-    return [weight_to_float(weight), units]
-
-
-def weight_to_float(weight: str):
-    weight = weight.replace(',', '.')
-    try:
-        weight = float(weight)
-    except:
-        if weight.find('х') != -1:
-            x, y = weight.split('х')
-        elif weight.find('x') != -1:
-            x, y = weight.split('x')
-        elif weight.find('*') != -1:
-            x, y = weight.split('*')
-        else:
-            return
-        weight = float(x) * float(y)
-    return weight
-
-
-def price_per_kg(units: str, price: float, weight: float) -> float:
-    kilo = 1000 if units == 'г' else 1
-    cost = price / weight * kilo
-    return cost
+from app.parser.auchan import Auchan
+from app.parser.fozzy import Fozzy
+from app.parser.metro import Metro
+from app.parser.tavriav import Tavriav
